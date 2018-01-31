21986c318697e245e4d24d12ddbfb1be617e2b26
Christopher Kriens <ckriens@brighthealthplan.com>
2018-01-30 20:32:30 -0600

    Initial commit

---
+++ FaceMask/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FaceMask
+//
+//  Created by Christopher Kriens on 1/30/18.
+//  Copyright © 2018 Christopher Kriens. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,24 @@
+
+#### Description
+
+###### Subtitle
+
+## Overview
+
+
+## Preview
+
+
+## Usage
+```swift
+```
+
+## Performance
+
+
+## Source Versioning
+* Xcode 9.2
+* iOS SDK 11.2
+* Swift 4
+
+## Credit
